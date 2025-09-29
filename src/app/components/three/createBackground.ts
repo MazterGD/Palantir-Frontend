f91932375d30f6701af497c80cb2e19cfc4987c4
BALASOORIYA K.B.R.D. <kbrashmikadushan@gmail.com>
2025-09-30 02:23:25 +0530

    fix: Mobile crashing fixed

--- src/app/components/three/createBackground.ts
+++ src/app/components/three/createBackground.ts
@@ -2,7 +2,12 @@ import * as THREE from "three";
 
 export async function addStarsBackground(scene: THREE.Scene) {
   const loader = new THREE.TextureLoader();
-  const texture = await loader.loadAsync("/textures/Stars/8k_stars_milky_way.webp");
+        const isMobile =
+    /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ||
+    (window.matchMedia && window.matchMedia("(max-width: 768px)").matches);
+
+    const texturePath = isMobile ? "/textures/Stars/4k_stars_milky_way.webp" : "/textures/Stars/8k_stars_milky_way.webp";
+  const texture = await loader.loadAsync(texturePath);
 
   texture.mapping = THREE.EquirectangularReflectionMapping;
 
